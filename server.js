16246d4a984830e9c1e844480fba0fcb97644c82
Jerry Wang <focaaby@gmail.com>
2016-04-27 22:41:54 +0800

    added serve-index module to route directory

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var express = require('express')
-var rewrite = require('express-urlrewrite')
+var serveIndex = require('serve-index')
 var fs = require('fs')
 var path = require('path')
 
 var app = express()
 
-fs.readdirSync(__dirname).forEach(function (file) {
-  if (fs.statSync(path.join(__dirname, file)).isDirectory())
-    app.use(rewrite('/' + file + '/*', '/' + file + './index.html'))
-})
-
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + "/"))
+app.use('/jquery', serveIndex(__dirname + '/jquery'));
 
 app.listen(8080, function () {
   console.log('Server listening on http://localhost:8080, Ctrl+C to stop')
